c04dca8d547bdc8f4ea48ecfb6d483516428ad48
CorinneSpruit <corinnemeijdam@hotmail.com>
2020-10-22 16:40:35 +0200

    Klaar

--- src/Auto.java
+++ src/Auto.java
@@ -1,47 +1,75 @@
 public class Auto {
 
-    static int snelheid = 0;
-    static String merk = "Toyota";
-    static int kilometerstand = 10000;
-    Auto auto1 = new Auto();
-    Auto auto2 = new Auto();
-    Auto auto3 = new Auto();
-
-    public static void main (String [] arg){
+    int snelheid;
+    String merk;
+     int kilometerstand = 1000;
 
 
-start ();
+    public static void main(String[] arg) {
 
-geefGas();
-geefGas();
-geefGas();
+        Auto auto1 = new Auto();
+        auto1.snelheid = 0;
+        auto1.merk = "Toyota";
+        //  auto1.getKilometerstand();
 
-stop();
-System.out.println("De kilometerstand van "+merk+ " is "+(getKilometerstand()));
+        Auto auto2 = new Auto();
+        auto2.snelheid = 10;
+        auto2.merk = "Volvo";
+
+
+        Auto auto3 = new Auto();
+        auto3.snelheid = 20;
+        auto3.merk = "Volkswagen";
+
+
+        auto1.start();
+        auto1.geefGas();
+        auto1.geefGas();
+        auto1.geefGas();
+        auto1.stop();
+
+        System.out.println("De kilometerstand van " + auto1.merk + " is " + (auto1.Kilometerstand()));
+
+        auto2.start();
+        auto2.geefGas();
+        auto2.geefGas();
+        auto2.geefGas();
+        auto2.stop();
+
+        System.out.println("De kilometerstand van " + auto2.merk + " is " + (auto2.Kilometerstand()));
+
+        auto3.start();
+        auto3.geefGas();
+        auto3.geefGas();
+        auto3.geefGas();
+        auto3.stop();
+
+
+        System.out.println("De kilometerstand van " + auto3.merk + " is " + (auto3.Kilometerstand()));
 
 
     }
 
-    private static int getKilometerstand() {
-        return kilometerstand;
+    int Kilometerstand() {
+
+        return kilometerstand;}
+
+
+        void start () {
+            System.out.println("De " + merk + " start");
+        }
+
+        void stop () {
+            System.out.println("De " + merk + " stopt");
+
+        }
+
+        void geefGas () {
+            snelheid += 20;
+            kilometerstand += 20;
+            //   System.out.println("De "+merk+" rijdt");
+            System.out.println("De " + merk + " heeft snelheid " + snelheid);
+
+        }
 
     }
-
-    static void start(){
-        System.out.println("De "+merk+" start");
-    }
-
-    static void stop(){
-        System.out.println("De "+merk+" stopt");
-
-    }
-
-    static void geefGas(){
-        snelheid+=20;
-        kilometerstand+=20;
-     //   System.out.println("De "+merk+" rijdt");
-        System.out.println("De "+merk+ " heeft snelheid "+snelheid);
-
-    }
-
-}
