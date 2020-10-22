64deae2aa7a99cfaee51b8b3189d982c2190e856
CorinneSpruit <corinnemeijdam@hotmail.com>
2020-10-22 14:40:36 +0200

    Klaar :)

--- src/Auto.java
+++ src/Auto.java
@@ -1,2 +1,47 @@
 public class Auto {
+
+    static int snelheid = 0;
+    static String merk = "Toyota";
+    static int kilometerstand = 10000;
+    Auto auto1 = new Auto();
+    Auto auto2 = new Auto();
+    Auto auto3 = new Auto();
+
+    public static void main (String [] arg){
+
+
+start ();
+
+geefGas();
+geefGas();
+geefGas();
+
+stop();
+System.out.println("De kilometerstand van "+merk+ " is "+(getKilometerstand()));
+
+
+    }
+
+    private static int getKilometerstand() {
+        return kilometerstand;
+
+    }
+
+    static void start(){
+        System.out.println("De "+merk+" start");
+    }
+
+    static void stop(){
+        System.out.println("De "+merk+" stopt");
+
+    }
+
+    static void geefGas(){
+        snelheid+=20;
+        kilometerstand+=20;
+     //   System.out.println("De "+merk+" rijdt");
+        System.out.println("De "+merk+ " heeft snelheid "+snelheid);
+
+    }
+
 }
